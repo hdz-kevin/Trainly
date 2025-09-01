6a8b6ae2d41ae4061007b48c722c91a4f2bb9870
hdz-kevin <kevinhdez592@gmail.com>
2025-09-01 11:56:58 -0600

    login user

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col gap-6">
-    <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
+    <x-auth-header :title="__('Inicia sesión en tu cuenta')" :description="__('Ingresa tus credenciales')" />
 
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
@@ -8,9 +8,8 @@
         <!-- Email Address -->
         <flux:input
             wire:model="email"
-            :label="__('Email address')"
+            :label="__('Correo Electrónico')"
             type="email"
-            required
             autofocus
             autocomplete="email"
             placeholder="email@example.com"
@@ -20,11 +19,10 @@
         <div class="relative">
             <flux:input
                 wire:model="password"
-                :label="__('Password')"
+                :label="__('Contraseña')"
                 type="password"
-                required
                 autocomplete="current-password"
-                :placeholder="__('Password')"
+                :placeholder="__('Contraseña')"
                 viewable
             />
 
@@ -36,17 +34,17 @@
         </div>
 
         <!-- Remember Me -->
-        <flux:checkbox wire:model="remember" :label="__('Remember me')" />
+        <flux:checkbox wire:model="remember" :label="__('Recordarme')" />
 
         <div class="flex items-center justify-end">
-            <flux:button variant="primary" type="submit" class="w-full">{{ __('Log in') }}</flux:button>
+            <flux:button variant="primary" type="submit" class="w-full">{{ __('Iniciar sesión') }}</flux:button>
         </div>
     </form>
 
     @if (Route::has('register'))
         <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
-            <span>{{ __('Don\'t have an account?') }}</span>
-            <flux:link variant="subtle" :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
+            <span>{{ __('¿No tienes una cuenta?') }}</span>
+            <flux:link variant="subtle" :href="route('register')" wire:navigate>{{ __('Regístrate') }}</flux:link>
         </div>
     @endif
 </div>
